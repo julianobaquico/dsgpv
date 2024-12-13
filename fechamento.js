cd2f53e7363257a35058e8f297c643e779ced1e9
Juliano Baquico <julianoandersondeassis@gmail.com>
2024-12-13 10:49:20 -0300

    sdfsd

--- fechamento.js
+++ fechamento.js
@@ -1,25 +1,22 @@
 const valores = [0.05, 0.10, 0.25, 0.50, 1.00, 2.00, 5.00, 10.00, 20.00];
-const quantidadesInputs = document.querySelectorAll('.quantidades input');
+const quantidadesInputs = document.querySelectorAll('.quantidade');
 const resultadosParciaisDiv = document.querySelector('.resultados-parciais');
 const valorTotalDiv = document.getElementById('valor-total');
 const calcularButton = document.getElementById('calcular');
 
-
 calcularButton.addEventListener('click', () => {
     let total = 0;
-    resultadosParciaisDiv.innerHTML = ''; // Limpa os resultados anteriores
+    resultadosParciaisDiv.innerHTML = '';
 
     quantidadesInputs.forEach((input, index) => {
         const quantidade = parseInt(input.value) || 0;
         const resultadoParcial = valores[index] * quantidade;
         total += resultadoParcial;
 
-       // Cria um novo div para o resultado parcial e o adiciona à div resultados-parciais
         const resultadoDiv = document.createElement('div');
         resultadoDiv.textContent = resultadoParcial.toFixed(2);
         resultadosParciaisDiv.appendChild(resultadoDiv);
     });
-
 
     valorTotalDiv.textContent = total.toFixed(2);
 
